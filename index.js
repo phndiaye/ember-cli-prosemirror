17ebd3d036dd42125802a1bfec5d2229b297d338
Philippe Ndiaye <phndiaye@gmail.com>
2020-12-27 19:41:37 +0100

    add the font awesome webfonts to the host apps

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 'use strict';
 
+const mergeTrees = require('broccoli-merge-trees');
+const Funnel = require('broccoli-funnel');
+
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  included() {
+    this._super.included.apply(this, arguments);
+
+    this.import('node_modules/@fortawesome/fontawesome-free/css/all.min.css');
+  },
+
+  treeForPublic() {
+    let publicTree = this._super.treeForPublic.apply(this, arguments);
+    let trees = [];
+
+    if (publicTree) {
+      trees.push(publicTree);
+    }
+
+    trees.push(
+      new Funnel('node_modules/@fortawesome/fontawesome-free/webfonts/', {
+        srcDir: '/',
+        destDir: 'webfonts'
+      })
+    );
+
+    return mergeTrees(trees);
+  }
 };
